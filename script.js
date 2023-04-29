63e3f52c81186701f6b82d66efd073c303960fc5
dotmidi <0994510@hr.nl>
2023-04-29 23:44:20 +0200

    balls

---
+++ script.js
@@ -0,0 +1,17 @@
+var startBtn = document.getElementById("star");
+var balls = new Audio("./sounds/button.mp3");
+var bgm = new Audio("./sounds/bgm.mp3");
+
+function func() {
+  balls.play();
+}
+
+// make a function called playBGM that plays the background music or pause it
+function playBGM() {
+  if (bgm.paused) {
+    bgm.play();
+  } else {
+    bgm.pause();
+    bgm.currentTime = 0;
+  }
+}
