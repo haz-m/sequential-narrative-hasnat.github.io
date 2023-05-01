6ae44d9252f83ad2d29d8a7bdff24580fc795a18
dotmidi <0994510@hr.nl>
2023-05-01 22:33:33 +0200

    interim v2

--- game/game.js
+++ game/game.js
@@ -2,13 +2,28 @@
 // if the last image is reached, start over at the beginning
 // use the images in the ../images folder
 var index = 0;
+var cameraClick = new Audio("../sounds/cameraClick.mp3");
+var bgm = new Audio("../sounds/bgm.mp3");
+
 function changeImage () {
+  // count all the number of files in the Scenes folder
+  var numFiles = 9
   var img = document.getElementById('sceneImage')
-  var imageArray = ['../images/4.png', '../images/4.png', '../images/5.png', '../images/6.png']
+  var imageArray = ['../images/Transition/trans1.png', '../images/Transition/trans2.png', '../images/Transition/trans3.png', '../images/Scenes/scene1.png', '../images/Scenes/scene2.png', '../images/Scenes/scene3.png', '../images/Scenes/scene4.gif', '../images/Scenes/scene5.png', '../images/Scenes/scene6.png']
   if (index === imageArray.length - 1) {
-    index = 4
-  } if(index == 4){
+    index = numFiles - 1
+  } if(index == numFiles - 1){
     window.location.href = "../index.html"
+  } if(index == 0){
+    cameraClick.play();
+    console.log("camera click")
+    index++
+  } if (index == 2){
+    bgm.play();
+    bgm.loop = true;
+    cameraClick.pause();
+    console.log("bgm play")
+    index++
   }
   else {
     index++
@@ -16,3 +31,4 @@ function changeImage () {
   img.src = imageArray[index]
   console.log(index)
 }
+
