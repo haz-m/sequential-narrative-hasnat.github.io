98c1219f9f5c2b5b86f5e702149e3582f7723e12
dotmidi <0994510@hr.nl>
2023-05-01 22:43:10 +0200

    fix 2

--- game/game.js
+++ game/game.js
@@ -1,6 +1,6 @@
 // write a function called changeImage() that, when the window is clicked, changes to the next image in the array
 // if the last image is reached, start over at the beginning
-// use the images in the ../images folder
+// use the Images in the ../Images folder
 var index = 0;
 var cameraClick = new Audio("../sounds/cameraClick.mp3");
 var bgm = new Audio("../sounds/bgm.mp3");
@@ -9,7 +9,7 @@ function changeImage () {
   // count all the number of files in the Scenes folder
   var numFiles = 9
   var img = document.getElementById('sceneImage')
-  var imageArray = ['../images/Transition/trans1.png', '../images/Transition/trans2.png', '../images/Transition/trans3.png', '../images/Scenes/scene1.png', '../images/Scenes/scene2.png', '../images/Scenes/scene3.png', '../images/Scenes/scene4.gif', '../images/Scenes/scene5.png', '../images/Scenes/scene6.png']
+  var imageArray = ['../Images/Transition/trans1.png', '../Images/Transition/trans2.png', '../Images/Transition/trans3.png', '../Images/Scenes/scene1.png', '../Images/Scenes/scene2.png', '../Images/Scenes/scene3.png', '../Images/Scenes/scene4.gif', '../Images/Scenes/scene5.png', '../Images/Scenes/scene6.png']
   if (index === imageArray.length - 1) {
     index = numFiles - 1
   } if(index == numFiles - 1){
